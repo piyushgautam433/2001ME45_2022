526900859b515af1e63251b09a537ab8e7c82c4f
piyushgautam433 <95177905+piyushgautam433@users.noreply.github.com>
2022-09-14 21:26:32 +0530

    comments added in the code

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,36 +1,36 @@
 #HareKrishna
 import pandas as pd #panda is imported
-df = pd.read_csv(r"octant_input.csv") 
+df = pd.read_csv(r"octant_input.csv")  #input csv file imported
 #print(df)
 
 mod = 5000
-temp = mod
+temp = mod #temp variable to store values of mod
 
-avgu = df['U'].mean()
+avgu = df['U'].mean()  
 avgv = df['V'].mean()
 avgw = df['W'].mean()
-
+#avf of all values in U, V, W
 #print(avgu)
 #print(avgv)
 #print(avgw)
 
-df['U Avg']= ''
-df['V Avg']= ''
-df['W Avg']= ''
+df['U Avg']= '' #new column created
+df['V Avg']= '' #new column created
+df['W Avg']= '' #new column created
 
 df.loc[0,['U Avg']] = avgu 
 df.loc[0,['V Avg']] = avgv
 df.loc[0,['W Avg']] = avgw
 
 
-df['U1_avg'] = df['U']-avgu
-df['V1_avg'] = df['V']-avgv
-df['W1_avg'] = df['W']-avgw
+df['U1_avg'] = df['U']-avgu  #subtracting cell value with avg U
+df['V1_avg'] = df['V']-avgv  #subtracting cell value with avg V
+df['W1_avg'] = df['W']-avgw  #subtracting cell value with avg W
 
 
 
 #print(df)
-
+#categorising values in different octants
 df.loc[((df.U1_avg>0) & (df.V1_avg>0) & (df.W1_avg>0)),"octant"] = "+1"
 df.loc[((df.U1_avg>0) & (df.V1_avg>0) & (df.W1_avg<0)),"octant"] = "-1"
 df.loc[((df.U1_avg<0) & (df.V1_avg>0) & (df.W1_avg>0)),"octant"] = "+2"
@@ -41,22 +41,20 @@ df.loc[((df.U1_avg>0) & (df.V1_avg<0) & (df.W1_avg>0)),"octant"] = "+4"
 df.loc[((df.U1_avg>0) & (df.V1_avg<0) & (df.W1_avg<0)),"octant"] = "-4"
 
 #print(df)
-########################################################### task 1 started
+################################################################ preprocessing done
+p1=0 #initialising value
+n1=0 #initialising value
+p2=0 #initialising value
+n2=0 #initialising value
+p3=0 #initialising value
+n3=0 #initialising value
+p4=0 #initialising value
+n4=0 #initialising value
 
-
-p1=0
-n1=0
-p2=0
-n2=0
-p3=0
-n3=0
-p4=0
-n4=0
-
-
+#for loop to counting the values
 for i in df['octant']:
     if i=="+1":
-        p1 = p1 +1
+        p1 = p1 +1 
     elif i == "-1":
         n1 = n1 + 1
     elif i == "+2":
@@ -81,82 +79,87 @@ print(n3)
 print(p4)
 print(n4)
 '''
+
+#empty column is created
 df[''] = ''
 
 df.loc[1,['']] = 'user input'
 
 
-df['Octant_id']= ''
-df['+1']= ''
-df['-1']= ''
-df['+2']= ''
-df['-2']= ''
-df['+3']= ''
-df['-3']= ''
-df['+4']= ''
-df['-4']= ''
+df['Octant_id']= '' #column created
+df['+1']= '' #column created
+df['-1']= '' #column created
+df['+2']= '' #column created
+df['-2']= '' #column created
+df['+3']= '' #column created
+df['-3']= '' #column created
+df['+4']= '' #column created
+df['-4']= '' #column created
 
 df.loc[1,['Octant_id']] = 'mod ' + str(temp)
 
 df.loc[0,['Octant_id']] = ['total count']
 
-df.loc[0,['+1']] = p1
-df.loc[0,['-1']] = n1
-df.loc[0,['+2']] = p2
-df.loc[0,['-2']] = n2
-df.loc[0,['+3']] = p3
-df.loc[0,['-3']] = n3
-df.loc[0,['+4']] = p4
-df.loc[0,['-4']] = n4
+df.loc[0,['+1']] = p1 #inserting values in dataframe
+df.loc[0,['-1']] = n1 #inserting values in dataframe
+df.loc[0,['+2']] = p2 #inserting values in dataframe
+df.loc[0,['-2']] = n2 #inserting values in dataframe
+df.loc[0,['+3']] = p3 #inserting values in dataframe
+df.loc[0,['-3']] = n3 #inserting values in dataframe
+df.loc[0,['+4']] = p4 #inserting values in dataframe
+df.loc[0,['-4']] = n4 #inserting values in dataframe
 
 #print(df)
 ############################################################ task 1 competed
+#print(df)
 
 #temp = input("enter the value of mod : ")
 
 
 #max_l = 30000
 
-len = len(df)
-i = 1
-begin = 0
-end = temp
+len = len(df) #to store total no. of rows
+i = 1 #loop to calculate mod counts
+begin = 0 #begin with zero
+end = temp #temp variable created
 
-while end<= len:
+while end<= len: #while loop started
     df['Octant_id'][i+1] = str(begin) + "-" + str(end-1)
-    l_1p=0
-    l_1n=0
-    l_2p=0
-    l_2n=0
-    l_3p=0
-    l_3n=0
-    l_4p=0
-    l_4n=0    
-    for j in range(begin,end): 
+    p1_1=0 #initialising value
+    n1_1=0 #initialising value
+    p2_1=0 #initialising value
+    n2_1=0 #initialising value
+    p3_1=0 #initialising value
+    n3_1=0 #initialising value
+    p4_1=0 #initialising value
+    n4_1=0 #initialising value  
+    for j in range(begin,end):
+        #counting the values 
         if df['octant'][j]=="+1":
-            l_1p = l_1p +1
+            p1_1 = p1_1 +1
         elif df['octant'][j] == "-1":
-            l_1n = l_1n + 1
+            n1_1 = n1_1 + 1
         elif df['octant'][j] == "+2":
-            l_2p = l_2p + 1
+            p2_1 = p2_1 + 1
         elif df['octant'][j] == "-2":
-            l_2n = l_2n + 1
+            n2_1 = n2_1 + 1
         elif df['octant'][j] == "+3":
-            l_3p = l_3p + 1
+            p3_1 = p3_1 + 1
         elif df['octant'][j] == "-3":
-            l_3n = l_3n + 1
+            n3_1 = n3_1 + 1
         elif df['octant'][j] == "+4":
-            l_4p = l_4p + 1
+            p4_1 = p4_1 + 1
         elif df['octant'][j] == "-4":
-            l_4n = l_4n + 1
-    df.loc[i+1,['+1']] = l_1p
-    df.loc[i+1,['-1']] = l_1n
-    df.loc[i+1,['+2']] = l_2p
-    df.loc[i+1,['-2']] = l_2n
-    df.loc[i+1,['+3']] = l_3p
-    df.loc[i+1,['-3']] = l_3n
-    df.loc[i+1,['+4']] = l_4p
-    df.loc[i+1,['-4']] = l_4n
+            n4_1 = n4_1 + 1
+    #storing the values in different locations
+    df.loc[i+1,['+1']] = p1_1
+    df.loc[i+1,['-1']] = n1_1
+    df.loc[i+1,['+2']] = p2_1
+    df.loc[i+1,['-2']] = n2_1
+    df.loc[i+1,['+3']] = p3_1
+    df.loc[i+1,['-3']] = n3_1
+    df.loc[i+1,['+4']] = p4_1
+    df.loc[i+1,['-4']] = n4_1
     begin = end
     i=i+1
     end = temp*i
@@ -165,37 +168,52 @@ while end<= len:
 
 if end>len:
     df['Octant_id'][i+1] = str(begin) + "-" + str(len-1)
-    p_1p=0
-    p_1n=0
-    p_2p=0
-    p_2n=0
-    p_3p=0
-    p_3n=0
-    p_4p=0
-    p_4n=0
+    p1_2=0 #initialising values
+    n1_2=0 #initialising values
+    p2_2=0 #initialising values
+    n2_2=0 #initialising values
+    p3_2=0 #initialising values
+    n3_2=0 #initialising values
+    p4_2=0 #initialising values
+    n4_2=0 #initialising values
     for j in range(begin,len):
+        #counting the values
         if df['octant'][j]=="+1":
-            p_1p = p_1p +1
+            p1_2 = p1_2 +1
         elif df['octant'][j] == "-1":
-            p_1n = p_1n + 1
+            n1_2 = n1_2 + 1
         elif df['octant'][j] == "+2":
-            p_2p = p_2p + 1
+            p2_2 = p2_2 + 1
         elif df['octant'][j] == "-2":
-            p_2n = p_2n + 1
+            n2_2 = n2_2 + 1
         elif df['octant'][j] == "+3":
-            p_3p = p_3p + 1
+            p3_2 = p3_2 + 1
         elif df['octant'][j] == "-3":
-            p_3n = p_3n + 1
+            n3_2 = n3_2 + 1
         elif df['octant'][j] == "+4":
-            p_4p = p_4p + 1
+            p4_2 = p4_2 + 1
         elif df['octant'][j] == "-4":
-            p_4n = p_4n + 1     
-    df.loc[i+1,['+1']] = p_1p
-    df.loc[i+1,['-1']] = p_1n
-    df.loc[i+1,['+2']] = p_2p
-    df.loc[i+1,['-2']] = p_2n
-    df.loc[i+1,['+3']] = p_3p
-    df.loc[i+1,['-3']] = p_3n
-    df.loc[i+1,['+4']] = p_4p
-    df.loc[i+1,['-4']] = p_4n   
+            n4_2 = n4_2 + 1     
+    #storing values in different locations
+    df.loc[i+1,['+1']] = p1_2
+    df.loc[i+1,['-1']] = n1_2
+    df.loc[i+1,['+2']] = p2_2
+    df.loc[i+1,['-2']] = n2_2
+    df.loc[i+1,['+3']] = p3_2
+    df.loc[i+1,['-3']] = n3_2
+    df.loc[i+1,['+4']] = p4_2
+    df.loc[i+1,['-4']] = n4_2   
+    
+    #file created using .csv function
+    df.to_csv("octant_output.csv")
+
+
+    ##################################################### task2 completed
+    
      
+
+
+     
+
+
+
