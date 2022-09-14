ec40a92a0e9f5a02c8c8d4bcbca0754e825a1166
piyushgautam433 <95177905+piyushgautam433@users.noreply.github.com>
2022-09-14 19:35:56 +0530

    Update tut01.py

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,14 +1,44 @@
-def octact_identification(mod=5000):
-###Code CS384
+#HareKrishna
+import pandas as pd #panda is imported
+df = pd.read_csv(r"octant_input.csv") 
+#print(df)
+
+mod = 5000
+temp = mod
+
+avgu = df['U'].mean()
+avgv = df['V'].mean()
+avgw = df['W'].mean()
+
+#print(avgu)
+#print(avgv)
+#print(avgw)
+
+df['U Avg']= ''
+df['V Avg']= ''
+df['W Avg']= ''
+
+df.loc[0,['U Avg']] = avgu
+df.loc[0,['V Avg']] = avgv
+df.loc[0,['W Avg']] = avgw
 
 
-from platform import python_version
-ver = python_version()
+df['U1_avg'] = df['U']-avgu
+df['V1_avg'] = df['V']-avgv
+df['W1_avg'] = df['W']-avgw
 
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
 
-mod=5000
-octact_identification(mod)
+
+#print(df)
+
+df.loc[((df.U1_avg>0) & (df.V1_avg>0) & (df.W1_avg>0)),"octant"] = "+1"
+df.loc[((df.U1_avg>0) & (df.V1_avg>0) & (df.W1_avg<0)),"octant"] = "-1"
+df.loc[((df.U1_avg<0) & (df.V1_avg>0) & (df.W1_avg>0)),"octant"] = "+2"
+df.loc[((df.U1_avg<0) & (df.V1_avg>0) & (df.W1_avg<0)),"octant"] = "-2"
+df.loc[((df.U1_avg<0) & (df.V1_avg<0) & (df.W1_avg>0)),"octant"] = "+3"
+df.loc[((df.U1_avg<0) & (df.V1_avg<0) & (df.W1_avg<0)),"octant"] = "-3"
+df.loc[((df.U1_avg>0) & (df.V1_avg<0) & (df.W1_avg>0)),"octant"] = "+4"
+df.loc[((df.U1_avg>0) & (df.V1_avg<0) & (df.W1_avg<0)),"octant"] = "-4"
+
+#print(df)
+################################################################ task 1 completed
