04d245a26bdbcfa710d3014057418d13a87a1d24
piyushgautam433 <95177905+piyushgautam433@users.noreply.github.com>
2022-09-29 12:07:34 +0530

    tut01 is updated

--- tut01/cal.py
+++
@@ -1,6 +0,0 @@
-from tkinter import Y
-
-
-def add(x, y):
-    print(x+y)
-    return x+y
--- tut01/super_cal.py
+++
@@ -1,7 +0,0 @@
-from tkinter import Y
-import cal
-add(x, y)
-    print("The sum pof x,y is: {}".format(x+y))
-    return x+y
-x = 3, y = 4
-sum = add(3, 4)
