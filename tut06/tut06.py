e919387f41c42b647925a7ae6b8337432239d9e6
piyushgautam433 <95177905+piyushgautam433@users.noreply.github.com>
2022-11-20 18:14:19 +0530

    input files imported

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -6,3 +6,15 @@ from datetime import datetime #import calender
 import openpyxl
 starting_time = datetime.now()
 
+def attendance_report(): #function 
+    try:
+        inp_file = pd.read_csv('input_attendance.csv') #attendence file important
+        inp = inp_file.fillna("Random") #Random in empty rows
+    except:
+        print("File not found")  
+    
+    
+    try:
+        rollno_inp=pd.read_csv('input_registered_students.csv')  #registered students file
+    except:
+        print('File containing name of all students is missing!')
